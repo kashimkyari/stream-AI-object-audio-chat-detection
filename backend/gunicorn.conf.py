3a5678b14e627c4130ee8830105cbcfe3567a0e9
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-28 19:38:12 +0100

    added scraper

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,27 +1,9 @@
-[Unit]
-Description=Gunicorn instance to serve my Flask app with SSL
-After=network.target
+import multiprocessing
 
-[Service]
-Type=simple
-User=ec2-user
-WorkingDirectory=/home/ec2-user/stream-AI-object-audio-chat-detection/backend
-
-# Use the gunicorn executable from the specified virtual environment
-# Added --certfile and --keyfile options to enable SSL.
-ExecStart=/opt/pytorch/bin/gunicorn --workers 12 --bind 0.0.0.0:5000 --certfile /home/ec2-user/certs/fullchain2.pem --keyfile /home/ec2-user/certs/privkey2.pem main:app
-
-Restart=always
-RestartSec=3
-KillMode=process
-TimeoutStartSec=30
-LimitNOFILE=4096
-
-# Set environment variables:
-# - VIRTUAL_ENV points to the root of your virtual environment.
-# - PATH is updated to prioritize the virtual environment's binaries.
-Environment=VIRTUAL_ENV=/source/opt/pytorch
-Environment=PATH=/source/opt/pytorch/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-[Install]
-WantedBy=multi-user.target
+bind = "0.0.0.0:5000"
+workers = multiprocessing.cpu_count() * 2 + 1  # Best practice
+threads = 2
+timeout = 60
+loglevel = "info"
+# accesslog = "/var/log/gunicorn/access.log"
+# errorlog = "/var/log/gunicorn/error.log"
