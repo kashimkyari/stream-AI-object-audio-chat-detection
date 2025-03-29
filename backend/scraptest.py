e1c6a3a32913688d286bdc022f3d101f49eb9072
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-29 04:01:53 +0100

    added ssl

--- backend/scraptest.py
+++ backend/scraptest.py
@@ -126,3 +126,40 @@ if __name__ == "__main__":
         print("Found m3u8 URL:", m3u8_url)
     else:
         print("No m3u8 URL found.")
+
+
+
+
+
+#   GNU nano 8.3                            /etc/systemd/system/.#gunicorn.servicecc76f0ae91d75cfd                                       
+# [Unit]
+# Description=Gunicorn instance to serve my Flask app with SSL
+# After=network.target
+
+# [Service]
+# Type=simple
+# User=ec2-user
+# WorkingDirectory=/home/ec2-user/stream-AI-object-audio-chat-detection/backend
+
+# # Use the gunicorn executable from the specified virtual environment
+# # Added --certfile and --keyfile options to enable SSL.
+# ExecStart=/opt/pytorch/bin/gunicorn --workers 6 --bind 127.0.0.1:5000 --certfile /home/ec2-user/certs/fullchain2.pem --keyfile /home/e>
+
+# Restart=always
+# RestartSec=3
+# KillMode=process
+# TimeoutStartSec=30
+# LimitNOFILE=4096
+
+# # Set environment variables:
+# # - VIRTUAL_ENV points to the root of your virtual environment.
+# # - PATH is updated to prioritize the virtual environment's binaries.
+# Environment=VIRTUAL_ENV=/source/opt/pytorch
+# Environment=PATH=/source/opt/pytorch/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# [Install]
+# WantedBy=multi-user.target
+
+#                                                            [ Read 27 lines ]
+# ^G Help         ^O Write Out    ^F Where Is     ^K Cut          ^T Execute  ^C Location     M-U Undo        M-A Set Mark
+# ^X Exit         ^R Read File    ^\ Replace  ^U Paste        ^J Justify  ^/ Go To Line   M-E Redo        M-6 Copy
