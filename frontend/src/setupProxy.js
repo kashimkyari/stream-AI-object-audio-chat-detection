8a83ac57fc86057a73ba9bd179723b6880cb6172
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-29 03:15:28 +0100

    added ssl

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'https://54.86.99.85:5000',
-            changeOrigin: true,
-            secure: false,  // Allow self-signed SSL certificates
-        })
-    );
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://54.86.99.85:5000", // Update with your Elastic IP
+      changeOrigin: true,
+      secure: true,
+    })
+  );
 };
