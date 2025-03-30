c11991f9c7f75d3bb630e0017f2fe555552aef47
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-30 01:13:41 +0100

    added proxy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
