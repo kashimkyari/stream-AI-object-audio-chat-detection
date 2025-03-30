60d39a35d193458ff6ecd597b7b899e62097d85c
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-30 01:20:13 +0100

    added analytics

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,7 @@ from cleanup import start_chat_cleanup_thread, start_detection_cleanup_thread
 from monitoring import start_notification_monitor
 import logging
 from flask_cors import CORS
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "https://stream-ai-object-audio-chat-detection.vercel.app/"}})
-
+CORS(app, resources={r"/api/*": {"origins": "*"}}) 
 
 with app.app_context():
     # Create all database tables if they do not exist.
