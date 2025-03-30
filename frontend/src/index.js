8225979c16b7bdc7058d7c6a598d7f80b45cfe1e
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-30 01:17:42 +0100

    added analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { Analytics } from "@vercel/analytics/react"
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
